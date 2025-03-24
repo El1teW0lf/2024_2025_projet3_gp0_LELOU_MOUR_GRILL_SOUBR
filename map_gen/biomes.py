6c733d1d40296dd0dbc314ce4c8233f30c23ce2b
GoldyRat <grillotcelestin@gmail.com>
2025-03-24 09:13:11 +0100

    biomes dictionnary

---
+++ map_gen/biomes.py
@@ -0,0 +1,44 @@
+biomes = {
+    "plains" : {
+        "resources" : {
+            "surface" : ["Fleurs"],
+            "underground" : ["Iron", "Coal", "Gold", "Redstone"]
+        }
+    },
+    "forest" : {
+        "resources" : {
+            "surface" : ["Fleurs", "Bois"],
+            "underground" : ["Iron", "Coal", "Gold", "Redstone"]    
+        }
+    },
+    "mountain" : {
+        "resources" : {
+            "surface" : [],
+            "underground" : ["Iron", "Coal", "Gold", "Redstone", "Diamond", "Emerald"]
+        }
+    },
+    "desert" : {
+        "resources" : {
+            "surface" : ["Sand", "Cactus"],
+            "underground" : ["Iron", "Coal", "Gold", "Redstone"]
+        }
+    },
+    "volcano" : {
+        "resources" : {
+            "surface" : ["Magma"],
+            "underground" : ["Magma"]
+        }
+    },
+    "snow" :{
+        "resources" : {
+            "surface" : ["Snow"],
+            "underground" : ["Iron", "Coal", "Gold", "Redstone", "Ice"]
+        }
+    },
+    "water" : {
+        "resources" : {
+            "surface" : ["Water", "Algae"],
+            "underground" : ["Water"]
+        }
+    }
+}
