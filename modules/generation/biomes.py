f8442cbe00d751b56c08e809c452d54aeb13b290
GoldyRat <grillotcelestin@gmail.com>
2025-03-24 09:22:10 +0100

    biomes dictionnary update with color, temperature and pop

--- modules/generation/biomes.py
+++ modules/generation/biomes.py
@@ -3,42 +3,64 @@ biomes = {
         "resources" : {
             "surface" : ["Fleurs"],
             "underground" : ["Iron", "Coal", "Gold", "Redstone"]
-        }
+        },
+        "color" : "#08ab17",
+        "temperature" : 20,
+        "population" : 50
     },
     "forest" : {
         "resources" : {
             "surface" : ["Fleurs", "Bois"],
             "underground" : ["Iron", "Coal", "Gold", "Redstone"]    
-        }
+        },
+        "color" : "#1c7925",
+        "temperature" : 18,
+        "population" : 40
     },
     "mountain" : {
         "resources" : {
             "surface" : [],
             "underground" : ["Iron", "Coal", "Gold", "Redstone", "Diamond", "Emerald"]
-        }
+        },
+        "color" : "#777777",
+        "temperature" : 10,
+        "population" : 10
     },
     "desert" : {
         "resources" : {
             "surface" : ["Sand", "Cactus"],
             "underground" : ["Iron", "Coal", "Gold", "Redstone"]
-        }
+        },
+        "color" : "#e2eba4",
+        "temperature" : 30,
+        "population" : 5
+        
     },
     "volcano" : {
         "resources" : {
             "surface" : ["Magma"],
             "underground" : ["Magma"]
-        }
+        },
+        "color" : "#561818",
+        "temperature" : 50,
+        "population" : 0
     },
     "snow" :{
         "resources" : {
             "surface" : ["Snow"],
             "underground" : ["Iron", "Coal", "Gold", "Redstone", "Ice"]
-        }
+        },
+        "color" : "#f4f4f4",
+        "temperature" : -10,
+        "population" : 0
     },
     "water" : {
         "resources" : {
             "surface" : ["Water", "Algae"],
             "underground" : ["Water"]
-        }
+        },
+        "color" : "#009eff",
+        "temperature" : 15,
+        "population" : 0
     }
 }
