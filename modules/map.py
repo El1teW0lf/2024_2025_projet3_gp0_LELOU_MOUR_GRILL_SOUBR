24dcc1c6ca904c81777e3e268228a9f725a22e67
Herasium <soubrievictor@gmail.com>
2025-04-04 13:11:36 +0200

    smorth

--- modules/map.py
+++ modules/map.py
@@ -11,9 +11,31 @@ class Map():
         self.noise = generate_worley_noise(num_points = 100)
         self._populate_map()
 
+        for i in range(5):
+            self._smooth()
+
+
     
     def _populate_map(self):
         for y in range(self.map.shape[0]):
             for x in range(self.map.shape[1]): 
                 self.map[y, x] = Tile(y,x,biomes[int(self.noise[y,x])])
                 self.map[y, x].setup()
+
+    def _smooth(self):
+        h, w = self.map.shape
+        for y in range(h):
+            for x in range(w):
+                neighbors = [
+                    self.map[ny][nx].biome
+                    for ny, nx in [(y-1, x), (y+1, x), (y, x-1), (y, x+1)]
+                    if 0 <= ny < h and 0 <= nx < w
+                ]
+
+                if neighbors:
+                    counts = {}
+                    for val in neighbors:
+                        counts[val] = counts.get(val, 0) + 1
+                    most_common = max(counts, key=counts.get)
+
+                    self.map[y][x].biome = most_common
